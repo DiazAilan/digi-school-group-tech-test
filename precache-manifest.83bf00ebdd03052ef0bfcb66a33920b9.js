96859007f382a197a2262216950ab3ca9516d3d6
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2020-04-15 23:03:58 +0200

    Updates

--- precache-manifest.83bf00ebdd03052ef0bfcb66a33920b9.js
+++ precache-manifest.83bf00ebdd03052ef0bfcb66a33920b9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e01d59b53e89a5da22064fcb1e756d19",
+    "revision": "4bb7a2a9ef70019935e6189a1fad78ed",
     "url": "/digi-school-group-tech-test/index.html"
   },
   {
-    "revision": "db882fc18660de4f6415",
+    "revision": "a3fa557f48c5ddaa1bfb",
     "url": "/digi-school-group-tech-test/static/css/main.789f69dd.chunk.css"
   },
   {
-    "revision": "f0cbece2b0882c6e3a46",
-    "url": "/digi-school-group-tech-test/static/js/2.ab5f331c.chunk.js"
+    "revision": "4b8c8883bf0a644381f2",
+    "url": "/digi-school-group-tech-test/static/js/2.9ef39024.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/digi-school-group-tech-test/static/js/2.ab5f331c.chunk.js.LICENSE.txt"
+    "url": "/digi-school-group-tech-test/static/js/2.9ef39024.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "db882fc18660de4f6415",
-    "url": "/digi-school-group-tech-test/static/js/main.2e8a42af.chunk.js"
+    "revision": "a3fa557f48c5ddaa1bfb",
+    "url": "/digi-school-group-tech-test/static/js/main.a736f6ac.chunk.js"
   },
   {
     "revision": "8ed00e080b081cec8182",
