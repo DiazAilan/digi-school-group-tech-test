06e45d8e4883df3213a3962e469909f4f5d771a1
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2020-04-15 17:27:40 +0200

    chore: enables /build to be distributed for github-pages

---
+++ build/precache-manifest.14b3f856b7995b56f5e80232e5500053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74eb21e6079f33fa840198e869c38c31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef2bcacbffc81909971",
+    "url": "/static/css/main.789f69dd.chunk.css"
+  },
+  {
+    "revision": "07e7f270433cd18117cf",
+    "url": "/static/js/2.eda550c7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eda550c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef2bcacbffc81909971",
+    "url": "/static/js/main.2efbae0f.chunk.js"
+  },
+  {
+    "revision": "7f0aba47f671d50864ab",
+    "url": "/static/js/runtime-main.d0b2243d.js"
+  }
+]);
